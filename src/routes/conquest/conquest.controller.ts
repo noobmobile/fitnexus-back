79f3d4c6ec48a8b4bf9820cc84cc787a90ee6a55
Luiz Eduardo Gomes Xavier <luiz.dudu3@gmail.com>
2024-09-05 09:55:32 -0300

    feat: add get my conquest endpoint

--- src/routes/conquest/conquest.controller.ts
+++ src/routes/conquest/conquest.controller.ts
@@ -2,13 +2,24 @@ import {
   Controller,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Get,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ConquestService } from './conquest.service';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../auth/models/UserRole';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { User } from '../user/entities/user.entity';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('conquest')
 @ApiTags('conquest')
 export class ConquestController {
   constructor(private readonly conquestService: ConquestService) {}
+
+  @Get('me')
+  @Roles(UserRole.User)
+  findMyPosts(@CurrentUser() user: User) {
+    return this.conquestService.findUserConquests(user.id);
+  }
 }
